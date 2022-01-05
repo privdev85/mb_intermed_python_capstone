9c23a5a9ca921b03e1084b78adf83355845ad303
privdev85 <privdev85@gmail.com>
2022-01-05 20:15:41 +0100

    current status

---
+++ extract.py
@@ -0,0 +1,77 @@
+"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+
+The `load_neos` function extracts NEO data from a CSV file, formatted as
+described in the project instructions, into a collection of `NearEarthObject`s.
+
+The `load_approaches` function extracts close approach data from a JSON file,
+formatted as described in the project instructions, into a collection of
+`CloseApproach` objects.
+
+The main module calls these functions with the arguments provided at the command
+line, and uses the resulting collections to build an `NEODatabase`.
+
+You'll edit this file in Task 2.
+"""
+import csv
+import json
+
+from models import NearEarthObject, CloseApproach
+
+
+def load_neos(neo_csv_path="data//neos.csv"):
+    """Read near-Earth object information from a CSV file.
+
+    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :return: A collection of `NearEarthObject`s.
+    """
+
+    neo_feat_dict = {
+        "designation": "pdes",
+        "name": "name",
+        "diameter": "diameter",
+        "hazardous": "pha",
+    }
+    neolist = []
+    with open(neo_csv_path) as csvfile:
+        neofile = csv.reader(csvfile, delimiter=",")
+        for i, row in enumerate(neofile):
+            if i == 0:
+                header = row
+                for key, val in neo_feat_dict.items():
+                    neo_feat_dict[key] = header.index(val)
+            else:
+                neolist.append(NearEarthObject(
+                    **{key: row[ind] for key, ind in neo_feat_dict.items()}
+                ))
+    return neolist
+
+
+def load_approaches(cad_json_path="data/cad.json"):
+    """Read close approach data from a JSON file.
+
+    :param cad_json_path: A path to a JSON file containing data about close approaches.
+    :return: A collection of `CloseApproach`es.
+    """
+    with open(cad_json_path) as json_file:
+        close_app = json.load(json_file)
+
+    cap_dict = {
+        "designation": "des",
+        "time": "cd",
+        "distance": "dist",
+        "velocity": "v_rel",
+    }
+
+    for feature in cap_dict.keys():
+        cap_dict[feature] = close_app["fields"].index(cap_dict[feature])
+
+    ### rename key to be consistent with Approach class attributes
+    cap_dict['_designation'] = cap_dict.pop('designation')
+
+    close_approach_coll = [
+        CloseApproach(
+            **{key: element[ind] for key, ind in cap_dict.items()}
+        )
+        for element in close_app["data"]
+    ]
+    return close_approach_coll
---
+++ helpers.py
@@ -0,0 +1,88 @@
+import pandas as pd
+
+"""Convert datetimes to and from strings.
+
+NASA's dataset provides timestamps as naive datetimes (corresponding to UTC).
+
+The `cd_to_datetime` function converts a string, formatted as the `cd` field of
+NASA's close approach data, into a Python `datetime`
+
+The `datetime_to_str` function converts a Python `datetime` into a string.
+Although `datetime`s already have human-readable string representations, those
+representations display seconds, but NASA's data (and our datetimes!) don't
+provide that level of resolution, so the output format also will not.
+"""
+import datetime
+import numpy as np
+
+
+def booltransform(obj):
+    transformdict = {"Y": True, "N": False, "":False, None: False} ### transforming None to False since test would otherwise not pass
+    if obj in transformdict.keys():
+        return transformdict[obj]
+    if isinstance(obj, bool) or obj in [0, 1]:
+        return bool(obj)
+    else:
+        raise ValueError("Cannot transform to Bool")
+
+
+transformdict = {
+    "str": lambda obj: str(obj) if obj not in ["",None] else None,
+    "float": lambda obj: float(obj) if obj not in ["", None] else float(np.nan),
+    "bool": lambda obj: booltransform(obj),
+}
+
+
+def coerce_input(obj, intended_type, transformdict, required=False):
+    try:
+        return transformdict[intended_type](obj)
+    except:
+        if required:
+            raise ValueError(
+                f"Required param not in correct form what object {obj} was passed with intended type {intended_type}"
+            )
+        else:
+            return None
+
+
+def cd_to_datetime(calendar_date):
+    """Convert a NASA-formatted calendar date/time description into a datetime.
+
+    NASA's format, at least in the `cd` field of close approach data, uses the
+    English locale's month names. For example, December 31st, 2020 at noon is:
+
+        2020-Dec-31 12:00
+
+    This will become the Python object `datetime.datetime(2020, 12, 31, 12, 0)`.
+
+    :param calendar_date: A calendar date in YYYY-bb-DD hh:mm format.
+    :return: A naive `datetime` corresponding to the given calendar date and time.
+    """
+    return datetime.datetime.strptime(calendar_date, "%Y-%b-%d %H:%M")
+
+
+def datetime_to_str(dt):
+    """Convert a naive Python datetime into a human-readable string.
+
+    The default string representation of a datetime includes seconds; however,
+    our data isn't that precise, so this function only formats the year, month,
+    date, hour, and minute values. Additionally, this function provides the date
+    in the usual ISO 8601 YYYY-MM-DD format to avoid ambiguities with
+    locale-specific month names.
+
+    :param dt: A naive Python datetime.
+    :return: That datetime, as a human-readable string without seconds.
+    """
+    return datetime.datetime.strftime(dt, "%Y-%m-%d %H:%M")
+
+def transform_obs_to_df(obs_list,idxname):
+        """
+        Transform NEO and approaches objects to a pandas dataframe.
+        """
+        df = pd.DataFrame([element.__dict__.values() for element in obs_list], columns=obs_list[0].__dict__.keys())
+        df[idxname] = df.index
+        return df
+
+
+def feature_to_index_dict(feature,obs_list):
+    return dict(zip([getattr(obj,feature) for obj in obs_list],range(0, len(obs_list))))
